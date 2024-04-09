27636119c5cb8452aaa3764c3367ab031de7e155
RedFront-site <sasha200509@mail.ru>
2024-04-09 09:14:26 +0300

    Добавьте файлы проекта.

---
+++ Socket_Client/Socket_Client.cpp
@@ -0,0 +1,127 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	WSADATA wsaData;
+	ADDRINFO hints;
+	ADDRINFO* addrResult = NULL;
+	SOCKET ConnectSocket = INVALID_SOCKET;
+
+
+	const char* sendBuffer = "Hello from Client!";
+	string message;
+	size_t BytesRead;
+
+	char recvBuffer[512];
+	char Buffer[1024];
+
+	int result;
+	// инициализация сокетов
+	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (result != 0) {
+		cout << "WSAStartup faild, result = " << result << endl;
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	result = getaddrinfo("localhost", "500", &hints, &addrResult);
+	if (result != 0) {
+		cout << "getaddrinfo faild, result = " << result << endl;
+		WSACleanup();
+		return 2;
+	}
+
+	ConnectSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+	if (ConnectSocket == INVALID_SOCKET) {
+		cout << "Socket creation faild" << endl;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 3;
+	}
+
+	result = connect(ConnectSocket, addrResult->ai_addr, (int) addrResult->ai_addrlen);
+	if (result == SOCKET_ERROR) {
+		cout << "Unable connect to server";
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 4;
+	}
+
+	cout << "Message: " << endl;
+	getline(cin, message);
+	
+	result = send(ConnectSocket,message.c_str(), (int)strlen(sendBuffer), 0);
+	if (result == SOCKET_ERROR) {
+		cout << "send faild, error" << result << endl;
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 5;
+	}
+
+	cout << "Bytes send: " << result << "bytes" << endl;
+
+	FILE* file;
+	if (fopen_s(&file, "test.txt", "rb") != 0){
+		cout << "Failed to open file\n";
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	while ((BytesRead = fread(Buffer, 1, sizeof(Buffer), file)) > 0) {
+		send(ConnectSocket, Buffer, static_cast<int>(BytesRead), 0);
+	}
+
+	// Close file
+	fclose(file);
+
+	result = shutdown(ConnectSocket, SD_SEND);
+	if (result == SOCKET_ERROR) {
+		cout << "shutdown, error" << result << endl;
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 6;
+	}
+
+	ZeroMemory(recvBuffer, 512);
+	do 
+	{
+
+		result = recv(ConnectSocket, recvBuffer, 512, 0);
+		if (result > 0) {
+			cout << "Receveid " << result << "bytes" << endl;
+			cout << "Receveid data: " << recvBuffer << endl;
+		}
+		else if (result == 0) {
+			cout << "Connection closed" << endl;
+		}
+		else {
+			cout << "recv faild with error" << endl;
+		}
+	} while (result > 0);
+
+	closesocket(ConnectSocket);
+	freeaddrinfo(addrResult);
+	WSACleanup();
+	return 0;
+}
+
---
+++ Socket_Server/Socet_Server.cpp
@@ -0,0 +1,153 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	//Объявление переменных
+	WSADATA wsaData;
+	ADDRINFO hints;
+	ADDRINFO* addrResult = NULL;
+	SOCKET ClientSocket = INVALID_SOCKET;
+	SOCKET ListenSocket = INVALID_SOCKET;
+
+	const char* sendBuffer = "The message has been delivered";
+
+	char recvBuffer[512];
+
+	int result;
+
+	// Инициализация сокетов
+	result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (result != 0) {
+		cout << "WSAStartup faild, result = " << result << endl;
+		return 1;
+	}
+
+	// Настройка hints
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+	
+	// Связываем сокет с адресом
+	result = getaddrinfo(NULL, "500", &hints, &addrResult);
+	if (result != 0) {
+		cout << "getaddrinfo faild, result = " << result << endl;
+		WSACleanup();
+		return 2;
+	}
+	// Cоздание сокета
+	ListenSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		cout << "Socket creation faild" << endl;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 3;
+	}
+
+	// Переводим сокет в режим прослушивания
+	result = bind(ListenSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen);
+	if (result == SOCKET_ERROR) {
+		cout << "Binding soket faild";
+		closesocket(ListenSocket);
+		ListenSocket = INVALID_SOCKET;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 4;
+	}
+
+	// Ожидаем подключения клиента
+	result = listen(ListenSocket, SOMAXCONN);
+	if (result == SOCKET_ERROR) {
+		cout << "Listening soket faild";
+		closesocket(ListenSocket);
+		ListenSocket = INVALID_SOCKET;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 5;
+	}
+
+	// Cоздаем сокет для Клиента
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+		cout << "Accepting socket faild" << endl;
+		closesocket(ListenSocket);
+		ListenSocket = INVALID_SOCKET;
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ListenSocket);
+
+	ZeroMemory(recvBuffer, 512);
+	do
+	{
+		result = recv(ClientSocket, recvBuffer, 512, 0);
+		if (result > 0) {
+			cout << "Receveid " << result << "bytes" << endl;
+			cout << "Receveid data: " << recvBuffer << endl;
+
+			result = send(ClientSocket, sendBuffer, (int)strlen(sendBuffer), 0);
+			if (result == SOCKET_ERROR){
+				cout << "Failed to send data back";
+				closesocket(ClientSocket);
+				freeaddrinfo(addrResult);
+				WSACleanup();
+				return 1;
+			}
+
+			FILE* file;
+			if (fopen_s(&file, "test.txt", "wb")!=0) {
+				std::cerr << "Failed to create file\n";
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+
+			char buffer[1024];
+			int bytesReceived;
+			do {
+				bytesReceived = recv(ClientSocket, buffer, sizeof(buffer), 0);
+				if (bytesReceived > 0) {
+					fwrite(buffer, 1, bytesReceived, file);
+				}
+			} while (bytesReceived > 0);
+
+			// Close file
+			fclose(file);
+		}
+		else if (result == 0) {
+			cout << "Connection closing..." << endl;
+		}
+		else {
+			cout << "recv faild with error" << endl;
+			closesocket(ClientSocket);
+			freeaddrinfo(addrResult);
+			WSACleanup();
+			return 1;
+		}
+	} while (result > 0);
+
+	result = shutdown(ClientSocket, SD_SEND);
+	if (result == SOCKET_ERROR) {
+		cout << "shutdown client socket faild" << endl;
+		closesocket(ClientSocket);
+		freeaddrinfo(addrResult);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(ClientSocket);
+	freeaddrinfo(addrResult);
+	WSACleanup();
+	return 0;
+}
